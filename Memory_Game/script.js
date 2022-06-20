3724bd913e97ac38f22abf87a4375fd28c062299
igrik56 <igrik1991@gmail.com>
2022-06-20 00:36:51 -0400

    updated

---
+++ Memory_Game/script.js
@@ -0,0 +1,150 @@
+const gameContainer = document.getElementById("game");
+
+const COLORS = [
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple",
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple"
+];
+
+// here is a helper function to shuffle an array
+// it returns the same array with values shuffled
+// it is based on an algorithm called Fisher Yates if you want ot research more
+function shuffle(array) {
+  let counter = array.length;
+
+  // While there are elements in the array
+  while (counter > 0) {
+    // Pick a random index
+    let index = Math.floor(Math.random() * counter);
+
+    // Decrease counter by 1
+    counter--;
+
+    // And swap the last element with it
+    let temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+
+  return array;
+}
+
+let shuffledColors = shuffle(COLORS);
+
+// this function loops over the array of colors
+// it creates a new div and gives it a class with the value of the color
+// it also adds an event listener for a click for each card
+function createDivsForColors(colorArray) {
+  for (let color of colorArray) {
+    // create a new div
+    const newDiv = document.createElement("div");
+
+    // give it a class attribute for the value we are looping over
+    newDiv.classList.add(color);
+
+    // call a function handleCardClick when a div is clicked on
+    newDiv.addEventListener("click", handleCardClick);
+
+    // append the div to the element with an id of game
+    gameContainer.append(newDiv);
+  }
+}
+
+
+
+// TODO: Implement this function!
+let a = undefined;
+let b = undefined;
+let clickCount = 0;
+const gameBoard = document.querySelector('#game');
+let cards = document.querySelector('#game').children;
+startBtn = document.querySelector("#Start");
+let cardCount = 0;
+
+function resetPick(){
+  a = undefined;
+  b = undefined;
+  clickCount = 0;
+}
+
+function noMatch(x, y){
+  console.log(`no match`);                                                            //if does not match close both cards.
+  //console.log (x+` `+y);
+  let dotA = x.indexOf('.');
+  document.querySelector('div.'+x).className = x.slice(0, dotA);
+  let dotB = y.indexOf(`.`);
+  document.querySelector('div.'+y).className = y.slice(0, dotB);
+}
+
+function startGame(){
+  console.log(`start new game`);
+  while(gameBoard.firstChild){
+    gameBoard.removeChild(gameBoard.firstChild);
+  }
+  shuffle(COLORS);
+  createDivsForColors(shuffledColors);
+}
+  // will be a restart same game function.
+  //   if(cards[i].classList.length > 1){
+  //     let x = cards[i].className.indexOf(` `);
+  //     cards[i].className = cards[i].className.slice(0, x);
+  //   }
+
+
+function handleCardClick(event) {
+  // you can use event.target to see which element was clicked
+  //console.log("you just clicked", event.target);
+  if(event.target.classList.length !== 2){
+    
+    /*clickCount++;                                 //clickCounter
+    if(clickCount == 2){
+      console.log(`more than 2 clicks`);
+      resetPick();
+    }
+    */
+    event.target.classList.add(`${event.target.className}-open`);
+    if(a === undefined){  
+      a = event.target.className
+      a = a.replace(` `,'.');
+    }
+    else if(b === undefined){
+      b = event.target.className;
+      b = b.replace(` `,'.');
+      //console.log(`${a} ${b}`);
+      
+      if(a !== b){
+        let timer = setTimeout(() => {
+          noMatch(a, b);
+          resetPick();
+          clearTimeout(timer);     
+        }, 400);
+      }
+      else{
+        console.log(`Match!`);
+        cardCount +=2;
+        resetPick();
+      }
+    }
+  }
+  if(cardCount == cards.length){
+    setTimeout(() => {
+      alert('Congratz! You won!');
+      startGame();
+    }, 600);
+  }
+}
+startBtn.onClick = startGame;
+
+// when the DOM loads
+document.addEventListener('DOMContentLoaded', function(e){
+  createDivsForColors(shuffledColors);
+});
+
+/* */
